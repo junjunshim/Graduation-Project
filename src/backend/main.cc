f5d4878bb762a495ceccc61e23109997580dd314
sdj202207004 <shimdj0425@gmail.com>
2026-03-10 09:58:44 +0900

    Feat : 서버 메인 프로세스 로직 변경

--- src/backend/main.cc
+++ src/backend/main.cc
@@ -3,25 +3,46 @@
 #include <cstdlib>
 
 int main() {
-    // 1. 환경 변수에서 DB 설정 읽어오기
-    std::string dbHost = std::getenv("DB_HOST") ? std::getenv("DB_HOST") : "db";
-    std::string dbPort = std::getenv("DB_PORT") ? std::getenv("DB_PORT") : "5432";
-    std::string dbName = std::getenv("DB_NAME") ? std::getenv("DB_NAME") : "grad_db";
-    std::string dbUser = std::getenv("DB_USER") ? std::getenv("DB_USER") : "admin";
-    std::string dbPass = std::getenv("DB_PASSWORD") ? std::getenv("DB_PASSWORD") : "admin123";
+    // 1. 설정 파일 로드(config.yaml)
+    drogon::app().loadConfigFile("/app/output.yaml");
+    LOG_INFO << "Server is starting based on config.json...";
 
-    // 2. DB 연결 설정 (PostgreSQL)
-    drogon::app().createDbClient("postgresql", dbHost, std::stoi(dbPort), dbName, dbUser, dbPass);
+    // 2. 서버 접속 테스트용 핸들러 등록
+    drogon::app().registerHandler("/server-test", [](const drogon::HttpRequestPtr&, 
+                                        std::function<void (const drogon::HttpResponsePtr &)> &&callback) {
+        auto resp = drogon::HttpResponse::newHttpResponse();
+        resp->setBody("TEST => Server is running ");
+        callback(resp);
+    });
+    LOG_INFO << "Server testing handler";
     
-    LOG_INFO << "Connecting to Database at " << dbHost << ":" << dbPort;
+    // 3. DB 접속 테스트용 핸들러 등록
+    drogon::app().registerHandler("/db-test", [](const drogon::HttpRequestPtr& req, 
+                                                std::function<void (const drogon::HttpResponsePtr &)> &&callback) {
+        auto dbClient = drogon::app().getDbClient(); // 'default' 클라이언트 호출
 
-    // 3. 서버 실행 포트 및 호스트 설정 (0.0.0.0으로 해야 컨테이너 밖에서 접속 가능)
-    drogon::app().addListener("0.0.0.0", 8080);
+        // PostgreSQL에 현재 시간을 물어보는 단순 쿼리 실행
+        dbClient->execSqlAsync(
+            "SELECT now() as current_time",
+            [callback](const drogon::orm::Result &result) {
+                auto now = result[0]["current_time"].as<std::string>();
+                auto resp = drogon::HttpResponse::newHttpResponse();
+                resp->setBody("DB Connection Success! DB Current Time: " + now);
+                callback(resp);
+            },
+            [callback](const drogon::orm::DrogonDbException &e) {
+                auto resp = drogon::HttpResponse::newHttpResponse();
+                resp->setStatusCode(drogon::k500InternalServerError);
+                resp->setBody("DB Connection Failed! Error: " + std::string(e.base().what()));
+                callback(resp);
+            }
+        );
+    });
+    LOG_INFO << "Database teseting handler";
 
-    LOG_INFO << "Server is running on http://localhost:8080";
 
-    // 5. 서버 가동
+    // 4. 서버 가동
     drogon::app().run();
-
+    
     return 0;
-}
+}   

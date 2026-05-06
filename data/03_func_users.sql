ece873c9e4707c2876d0b5d5840347679fa5f12d
junjunshim <shimdj0425@naver.com>
2026-05-06 09:37:14 +0900

    Data : 로그인, 회원가입관련 데이터베이스 함수 로직 변경

--- data/03_func_users.sql
+++ data/03_func_users.sql
@@ -6,10 +6,16 @@ CREATE OR REPLACE FUNCTION register_user(
     p_email users.email%TYPE,
     p_name users.name%TYPE,
     p_password_hash users.password_hash%TYPE
-) RETURNS VOID AS $$
+) RETURNS BOOLEAN AS $$
 DECLARE
     v_new_node_id organization_nodes.node_id%TYPE;
 BEGIN
+    -- 0. 사용자 중복 체크
+    IF EXISTS (SELECT 1 FROM users WHERE email = p_email) THEN
+        RAISE EXCEPTION '[P0501] Email already exists: %', p_email
+        USING ERRCODE = 'P0501';
+    END IF;
+
     -- 1. 유저 생성
     INSERT INTO users (user_id, email, name, password_hash)
     VALUES (p_user_id, p_email, p_name, p_password_hash);
@@ -40,6 +46,16 @@ BEGIN
 
     -- 6. 개인 노드에 대한 기본 권한 설정
     PERFORM default_node_authority(v_new_node_id);
+
+    -- 7. 성공적으로 사용자 등록 완료
+    RETURN TRUE;
+
+    EXCEPTION
+        WHEN SQLSTATE 'P0501' THEN
+        RAISE;
+        WHEN OTHERS THEN
+        RAISE EXCEPTION '[P0502] Error occurred while registering user: %', SQLERRM
+        USING ERRCODE = 'P0502';
 END;
 $$ LANGUAGE plpgsql;
 
@@ -51,33 +67,39 @@ CREATE OR REPLACE FUNCTION login_user (
     p_refresh_token user_refresh_tokens.refresh_token%TYPE,
     p_refresh_expiry user_refresh_tokens.expires_at%TYPE
 ) 
-RETURNS TABLE (
-    status BOOLEAN,
-    message TEXT
-) AS $$
+RETURNS BOOLEAN AS $$
 DECLARE
     v_user_password_hash users.password_hash%TYPE;
 BEGIN
+    -- 1. 이메일 존재 여부 확인 및 패스워드 해시값 검증
     SELECT password_hash INTO v_user_password_hash
     FROM users
     WHERE email = p_email;
 
     IF NOT FOUND THEN
-        RETURN QUERY SELECT FALSE, '사용자를 찾을 수 없습니다.'::TEXT;
-        RETURN;
+        RAISE EXCEPTION '[P0503] Email not found: %', p_email
+        USING ERRCODE = 'P0503';
     END IF;
 
     IF v_user_password_hash <> p_password_hash THEN
-        RETURN QUERY SELECT FALSE, '비밀번호가 일치하지 않습니다.'::TEXT;
-        RETURN;
+        RAISE EXCEPTION '[P0504] Incorrect password for email: %', p_email
+        USING ERRCODE = 'P0504';
     END IF;
 
+    -- 2. 로그인 성공 시 리프레시 토큰 저장 (업sert)
     INSERT INTO user_refresh_tokens (user_email, refresh_token, expires_at) 
     VALUES (p_email, p_refresh_token, p_refresh_expiry)
     ON CONFLICT (user_email) DO UPDATE SET
         refresh_token = EXCLUDED.refresh_token,
         expires_at = EXCLUDED.expires_at;
     
-    RETURN QUERY SELECT TRUE, '로그인 성공'::TEXT;
+    RETURN TRUE;
+
+    EXCEPTION
+        WHEN SQLSTATE 'P0503' OR SQLSTATE 'P0504' THEN
+        RAISE;
+        WHEN OTHERS THEN
+        RAISE EXCEPTION '[P0505] Error occurred during login: %', SQLERRM
+        USING ERRCODE = 'P0505';
 END;
 $$ LANGUAGE plpgsql;
